1d920f90299d655522c458eb403da357228100eb
JavohirJalilov <javohirjaliloff@gmail.com>
2023-07-02 12:41:48 +0500

    test

--- main.py
+++ main.py
@@ -2,24 +2,13 @@ import numpy as np
 import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
-from utils import (
-    load_model,
-    predict,
-    numpyToPIL,
-    PILToNumpy,
-    resizeImage
-)
+from utils import SegmentDocument
 # load image with PIL Image from data/document.jpg
-image = Image.open("data/document.jpg")
+image = Image.open("data/doc1.jpg")
 
+segment = SegmentDocument(image)
+
+doc = segment.segmentImage()
 # show image
-model, feature_extractor = load_model()
-logits = predict(model, feature_extractor, image)
-
-pred = logits[0][0]
-
-size = (image.size[0], image.size[1])
-pred = resizeImage(pred, size)
-
-# save image with numpy array
-plt.imsave("data/prediction.png", pred, cmap="gray")
+plt.imshow(doc, cmap="gray")
+plt.show()
