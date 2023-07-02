1a97d1fb73f7a123c1e7c6aa787ec93d827e3971
JavohirJalilov <javohirjaliloff@gmail.com>
2023-07-02 11:27:19 +0500

    added utils functions

---
+++ utils.py
@@ -0,0 +1,22 @@
+from transformers import SegformerFeatureExtractor, SegformerForSemanticSegmentation
+from PIL import Image
+import numpy as np
+
+def load_model():
+    feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
+    model = SegformerForSemanticSegmentation.from_pretrained(f"Jalilov/doc-segment")
+    return model, feature_extractor
+
+def predict(model, feature_extractor, image):
+    inputs = feature_extractor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    # convert to numpy array
+    logits = logits.detach().numpy()
+    return logits
+
+def numpyToPIL(image):
+    return Image.fromarray(image)
+
+def PILToNumpy(image):
+    return np.array(image)
