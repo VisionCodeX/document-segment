1d3a3ec513ca4f734a9bbe8b89dd2e10f0ea796d
JavohirJalilov <javohirjaliloff@gmail.com>
2023-07-02 12:41:37 +0500

    write class for document segmentation

--- utils.py
+++ utils.py
@@ -3,25 +3,32 @@ from PIL import Image
 import numpy as np
 import cv2
 
-def load_model():
-    feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
-    model = SegformerForSemanticSegmentation.from_pretrained(f"Jalilov/doc-segment")
-    return model, feature_extractor
+class SegmentDocument:
+    def __init__(self, image):
+        self.image = image
+        self.feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
+        self.model = SegformerForSemanticSegmentation.from_pretrained(f"Jalilov/doc-segment")
 
-def predict(model, feature_extractor, image):
-    inputs = feature_extractor(images=image, return_tensors="pt")
-    outputs = model(**inputs)
-    logits = outputs.logits
-    # convert to numpy array
-    logits = logits.detach().numpy()
-    return logits
-
-def numpyToPil(image):
-    return Image.fromarray(image)
-
-def pilToNumpy(image):
-    return np.array(image)
-
-def resizeImage(image, size):
-    return cv2.resize(image, size)
+    def predict(self):
+        inputs = self.feature_extractor(images=self.image, return_tensors="pt")
+        outputs = self.model(**inputs)
+        logits = outputs.logits
+        # convert to numpy array
+        logits = logits.detach().numpy()
+        return logits
     
+    def numpyToPil(self):
+        return Image.fromarray(self.image)
+    
+    def pilToNumpy(self):
+        return np.array(self.image)
+    
+    def resizeImage(self,pred, size):
+        return cv2.resize(pred, size)
+    
+    def segmentImage(self):
+        logits = self.predict()
+        pred = logits[0][0]
+        size = (self.image.size[0], self.image.size[1])
+        pred = self.resizeImage(pred, size)
+        return pred
