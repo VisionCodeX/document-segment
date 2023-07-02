670e12c773be99ae723b78534bf42d86e3713014
JavohirJalilov <javohirjaliloff@gmail.com>
2023-07-02 15:51:39 +0500

    update

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ import numpy as np
 import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
-from utils import SegmentDocument
+from utils import SegmentDocument, GetDocument
 # load image with PIL Image from data/document.jpg
 image = Image.open("data/doc1.jpg")
 
 segment = SegmentDocument(image)
 
+# segmentation document image
 doc = segment.segmentImage()
-# show image
-plt.imshow(doc, cmap="gray")
+
+# float32 to uint8
+doc = segment.float32ToUint8(doc)
+
+
+get_doc = GetDocument(doc, image)
+
+# image convert PIL to numpy
+image = np.array(image)
+
+transdormed = get_doc.transform(image)
+
+plt.imshow(doc)
 plt.show()
